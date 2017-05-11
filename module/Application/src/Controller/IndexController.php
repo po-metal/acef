9d48345016c2aa4a836344e79472e534517bdb4e
Alejandro Furgeri <alejandro.furgeri@sondeos.com.ar>
2017-05-11 18:00:35 -0300

    Profile y mail reset

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -11,4 +11,15 @@ class IndexController extends AbstractActionController
     {
         $this->redirect()->toRoute("Acef/Inicio/Main");
     }
+    
+    public function testAction(){
+        $view = new ViewModel();
+        $view->setTemplate('zf-metal/security/mail/reset');
+        $view->setVariables([
+            'newPassowrd'=> '123456',
+            'url' => $this->url()->fromRoute('zf-metal.user/login', [], ['force_canonical'=>true]),
+        ]);
+        
+        return $view;
+    }
 }
