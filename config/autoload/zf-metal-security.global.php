7a5c95e60690afe066b5c1c46db9cfffcb15129c
Alejandro Furgeri <alejandro.furgeri@sondeos.com.ar>
2017-05-11 01:38:50 -0300

    Se actualiza view profile

--- config/autoload/zf-metal-security.global.php
+++ config/autoload/zf-metal-security.global.php
@@ -7,7 +7,7 @@ return[
         'user_state_default' => true,
         'password_recovery' => true,
         'bcrypt_cost'=> 12,
-        'profile_picture_path' => __DIR__ . '/../public/img/profile/',
+        'profile_picture_path' => __DIR__ . '/../../public/img/profile/',
         'profile_picture_path_relative' => '/img/profile/',
         'redirect_strategy' => [
             'redirect_when_connected' => true,
