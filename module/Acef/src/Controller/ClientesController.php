e7949929597418dff3705d16772d0c0e4b7d6e9d
cristian <cristian@192.168.12.109>
2017-05-14 20:18:40 -0300

    Validator on Cliente

--- module/Acef/src/Controller/ClientesController.php
+++ module/Acef/src/Controller/ClientesController.php
@@ -13,8 +13,7 @@ use Zend\Mvc\Controller\AbstractActionController;
  * @license Creative Commons
  * @link https://github.com/zf-metal
  */
-class ClientesController extends AbstractActionController
-{
+class ClientesController extends AbstractActionController {
 
     const ENTITY = '\\Acef\\Entity\\Cliente';
 
@@ -28,45 +27,41 @@ class ClientesController extends AbstractActionController
      */
     public $grid = null;
 
-    public function getEm()
-    {
+    public function getEm() {
         return $this->em;
     }
 
-    public function setEm(\Doctrine\ORM\EntityManager $em)
-    {
+    public function setEm(\Doctrine\ORM\EntityManager $em) {
         $this->em = $em;
     }
 
-    public function __construct(\Doctrine\ORM\EntityManager $em, \ZfMetal\Datagrid\Grid $grid)
-    {
+    public function __construct(\Doctrine\ORM\EntityManager $em, \ZfMetal\Datagrid\Grid $grid) {
         $this->em = $em;
         $this->grid = $grid;
     }
 
-    public function getGrid()
-    {
+    public function getGrid() {
         return $this->grid;
     }
 
-    public function setGrid(\ZfMetal\Datagrid\Grid $grid)
-    {
+    public function setGrid(\ZfMetal\Datagrid\Grid $grid) {
         $this->grid = $grid;
     }
 
-    public function gridAction()
-    {
-       // $this->grid->addExtraColumn("Manager", "<a class=' table-link success' href='/acef/manager-cliente/main/{{id}}' ><span class='fa-stack'><i class='fa fa-square fa-stack-2x'></i><i class='fa fa-cog fa-stack-1x fa-inverse'></i></span></a>", "right", false);
+    public function gridAction() {
+
+        $this->grid->getSource()->getEventManager()->attach('buildCrudForm', function($e) {
+            $form = $e->getParam('form');
+            $form->add(new \Zend\Form\Element\Hidden("id"));
+        });
+
 
         $this->grid->prepare();
         return array("grid" => $this->grid);
     }
 
-    public function getEntityRepository()
-    {
+    public function getEntityRepository() {
         return $this->getEm()->getRepository(self::ENTITY);
     }
 
-
 }
-
