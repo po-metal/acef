8c63dc1538269a9742529630b7c06153440f9365
cristian <cristian@192.168.12.109>
2017-05-23 17:51:18 -0300

    Se oculta view de Productos y estados

--- module/Acef/config/zfm-datagrid.estado.config.php
+++ module/Acef/config/zfm-datagrid.estado.config.php
@@ -48,7 +48,7 @@ return [
                     'value' => 'Agregar Estado',
                 ],
                 'view' => [
-                    'enable' => true,
+                    'enable' => false,
                     'class' => ' table-link',
                     'value' => '<span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-search-plus fa-stack-1x fa-inverse"></i></span>',
                 ],
