3ce0b734007b1599889e38d5823e99ce5df881e9
Alejandro Furgeri <alejandro.furgeri@sondeos.com.ar>
2017-06-03 11:43:55 -0300

    Ajustes de perfil, validator tipo producto, permitir letras producto

--- module/Acef/config/custom-validators.config.php
+++ module/Acef/config/custom-validators.config.php
@@ -29,6 +29,19 @@ return [
                 ));
                 return $uniqueObject;
             },
+            'TipoProductoValidator' =>
+            function ($container) {
+                $uniqueObject = new \DoctrineModule\Validator\UniqueObject(array(
+                    'use_context' => true,
+                    'fields' => ['nombre'],
+                    'object_repository' => $container->get('doctrine.entitymanager.orm_default')->getRepository('Acef\Entity\TipoProducto'),
+                    'object_manager' => $container->get('doctrine.entitymanager.orm_default'),
+                    'messages' => [
+                        'objectNotUnique' => 'El producto ingresado ya existe en otro registro.'
+                    ]
+                ));
+                return $uniqueObject;
+            },
         ]
     ],
 ];
