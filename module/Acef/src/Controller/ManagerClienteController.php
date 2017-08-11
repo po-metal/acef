4f244ca09efb4b384046e6e845ee3bc273b9e35c
Alejandro Furgeri <alejandro.furgeri@sondeos.com.ar>
2017-08-10 23:07:42 -0300

    Update

--- module/Acef/src/Controller/ManagerClienteController.php
+++ module/Acef/src/Controller/ManagerClienteController.php
@@ -255,7 +255,7 @@ class ManagerClienteController extends AbstractActionController
 
         $grid->prepare();
 
-        $view = new \Zend\View\Model\ViewModel(array('grid' => $grid, 'deuda'=> $cliente->getDeuda(), 'deudaContable'=>$cliente->getDeudaContable()));
+        $view = new \Zend\View\Model\ViewModel(array('grid' => $grid,  'deuda'=>number_format($cliente->getDeuda(),0,',',''), 'deudaContable'=> number_format($cliente->getDeudaContable(),0,',','')));
 
         $view->setTerminal(TRUE);
 
@@ -309,7 +309,7 @@ class ManagerClienteController extends AbstractActionController
 
         $grid->prepare();
         
-        $view = new \Zend\View\Model\ViewModel(array('grid' => $grid, 'deuda'=>$cliente->getDeuda(), 'deudaContable'=>$cliente->getDeudaContable()));
+        $view = new \Zend\View\Model\ViewModel(array('grid' => $grid, 'deuda'=>number_format($cliente->getDeuda(),0,',',''), 'deudaContable'=> number_format($cliente->getDeudaContable(),0,',',''))) ;
 
         $view->setTerminal(TRUE);
 
